aeeda2ecbfebe5d3f8ede51862bfe98ba188e9e4
RomanSh <romashmarov82@gmail.com>
2024-01-05 05:45:21 +0300

    main page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
 import styles from '@/styles/modules/mainPage.module.scss';
 
 export default function Home() {
+
+
 	return (
 		<main className={styles.main}>
-			<h1 className={styles.h1}>Hello next js</h1>
 		</main>
-	)
+	);
 }
