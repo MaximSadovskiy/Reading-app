10f2f3afdceed8c187447b4abaa5fab1fa819c5f
Roman Shmarov <romashmarov82@gmail.com>
2024-01-31 03:28:39 +0300

    carousel developed

--- src/components/booksPage/client/SlideBtn.tsx
+++ src/components/booksPage/client/SlideBtn.tsx
@@ -2,18 +2,22 @@
 import { SlideArrowSvg } from "@/components/shared/Svg";
 import styles from "@/styles/modules/booksPage/slideBtn.module.scss";
 
+
 interface BtnProps {
     direction: 'left' | 'right';
-
+    handleClick: () => void;
+    isDisabled: boolean;
 }
 
-const SlideBtn = ({ direction }: BtnProps) => {
+
+const SlideBtn = ({ direction, handleClick, isDisabled }: BtnProps) => {
 
     const spanText = direction === 'left' ? 'назад' : 'вперёд';
+    
 
     return (
-        <button className={styles.slideBtn} data-direction={direction}
-            
+        <button className={`${styles.slideBtn} ${isDisabled ? styles.slideBtnDisabled : ''}`} data-direction={direction}
+            onClick={() => handleClick()}
         >
             {direction === 'right' && <span>{spanText}</span>}
             <SlideArrowSvg width={40} height={40} direction={direction} />
--- src/hooks/useSlideContainer.ts
+++ src/hooks/useSlideContainer.ts
@@ -1,83 +1,36 @@
 
-type createSlideContainerSign = (containerRef: React.RefObject<HTMLElement>, currentScrollRef: React.MutableRefObject<number>, cardCount: number, cardWidth: number, containerPadding: number) => { handlePrevClick: () => void, handleNextClick: () => void};
+/* получить желаемую дистанцию скролла */
+export const getScrollDistanceOnBreakpoint = (windowWidth: number, cardWidth: number, gapWidth: number) => {
 
-
-const howMuchCardScrollOnBreakpoint = (windowWidth: number, cardWidth: number, gapWidth: number) => {
-
-    let wantToScrollCardsCount: number;
+    /* want to scroll count, not All count */
+    let cardCount: number;
     let gapCount: number;
     
-    if (windowWidth < 550) {
-        wantToScrollCardsCount = 1;
-        gapCount = wantToScrollCardsCount - 1;
-    }
-    else if (windowWidth < 850) {
-        wantToScrollCardsCount = 2;
-        gapCount = wantToScrollCardsCount - 1;
+    if (windowWidth < 1150) {
+        cardCount = 1;
     }
     else {
-        wantToScrollCardsCount = 3;
-        gapCount = wantToScrollCardsCount - 1;
+        cardCount = 2;
     }
 
-    return wantToScrollCardsCount * cardWidth + gapCount * gapWidth;
-}
-
-
-const createSlideContainer: createSlideContainerSign = (containerRef, currentScrollRef, allCardCount, cardWidth = 220, gapWidth = 20) => {   
-
-    // helpers
-    const container = containerRef.current as HTMLUListElement;
-    const currentScroll = currentScrollRef.current as number;
-    const windowWidth = document.documentElement.clientWidth;
-
-    const allGapCount = allCardCount - 1;
-    const maxScrollWidth = allCardCount * cardWidth + allGapCount * gapWidth;
-    const wantToScrollWidth = howMuchCardScrollOnBreakpoint(windowWidth, cardWidth, gapWidth);
-    
-    /* event handlers */
-    const handlePrevClick = () => {
-        /* log */
-        console.log('prev click');
-        if (currentScroll < wantToScrollWidth) {
-            if (currentScroll > 0) {
-                container.style.transform = 'translateX(0)';
-                /* update in ref */
-                currentScrollRef.current = 0;
-            } else {
-                return;
-            }
-        }
-        else {
-            container.style.transform = `translateX(${-wantToScrollWidth}px)`;
-            /* update in ref */
-            currentScrollRef.current -= wantToScrollWidth;
-        }
-    };
-
-    const handleNextClick = () => {
-        /* log */
-        console.log('next click');
-        if (currentScroll + wantToScrollWidth > maxScrollWidth) {
-            const scrollToEndWidth = maxScrollWidth - currentScroll;
-            if (scrollToEndWidth > 0) {
-                container.style.transform = `translateX(${scrollToEndWidth}px)`;
-                /* update in ref */
-                currentScrollRef.current = maxScrollWidth;
-            }
-            else {
-                return;
-            }
-        }
-        else {
-            container.style.transform = `translateX(${wantToScrollWidth}px)`;
-            /* update in ref */
-            currentScrollRef.current += wantToScrollWidth;
-        }
-    }
-
-
-    return { handlePrevClick, handleNextClick };
+    gapCount = cardCount;
+    return (cardCount * cardWidth) + (gapCount * gapWidth)
 };
 
-export default createSlideContainer;
+
+/* получить дистанцию видмимых кард (внутри лист-контейнера) */
+export const getCurrentVisibleDistance = (windowWidth: number, cardWidth: number, gapWidth: number) => {
+    let cardCount: number, gapCount: number;
+
+    if (windowWidth < 550) cardCount = 1;
+    else if (windowWidth < 850) cardCount = 2;
+    else if (windowWidth < 1150) cardCount = 3;
+    else if (windowWidth < 1450) cardCount = 4;
+    else if (windowWidth < 1550) cardCount = 5;
+    else cardCount = 6;
+    
+
+    gapCount = cardCount - 1;
+
+    return (cardCount * cardWidth) + (gapCount * gapWidth)
+};
